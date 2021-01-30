506e7888cc18bed571a6ae358306fa8fb0eb1c0e
dqn <dqn270@gmail.com>
2021-01-31 07:02:28 +0900

    feat: implement demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,66 @@
 import * as React from "react";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { useNiconico } from "react-niconico";
+
+const sampleVideoUrl =
+  "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
 
 const Home: React.FC = () => {
-  return <main className="max-w-2xl mx-auto px-2 py-5">Hello!</main>;
+  const { register, handleSubmit, reset } = useForm();
+  const [videoRef, emitText] = useNiconico<HTMLVideoElement>();
+
+  const handleSubmitText = useCallback(
+    handleSubmit<{ text: string }>(({ text }) => {
+      emitText(text);
+      reset();
+    }),
+    [emitText, reset],
+  );
+
+  return (
+    <main className="max-w-2xl mx-auto px-2 py-5">
+      <h1 className="text-2xl font-bold">react-niconico demo</h1>
+      <div>
+        <video
+          ref={videoRef}
+          src={sampleVideoUrl}
+          className="w-full"
+          autoPlay
+          muted
+          controls
+          loop
+        />
+        <form onSubmit={handleSubmitText}>
+          <div className="flex">
+            <input
+              ref={register({ required: true })}
+              name="text"
+              type="text"
+              className="flex-1 border border-gray-400 px-2"
+              placeholder="enter text..."
+            />
+            <button
+              type="submit"
+              className="flex-shrink-0 text-white bg-blue-500 p-1"
+            >
+              POST
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-4">
+        <a
+          href="https://github.com/dqn/react-niconico"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 font-bold hover:underline"
+        >
+          react-niconico
+        </a>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
